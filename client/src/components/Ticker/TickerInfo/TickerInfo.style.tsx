2e049e8b2c5761203180b659022b984f75e59b30
Christiaan Seerden <christiaanseerden@live.nl>
2022-01-22 21:54:08 +0100

    (TickerInfo) create files

---
+++ client/src/components/Ticker/TickerInfo/TickerInfo.style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledTickerInfo = styled.div`
+    /* common section style here. 
+        share with NewTicket, TradeCard, etc.
+     */
+`;
--- client/src/components/Ticker/TickerInfo/TickerInfo.tsx
+++ client/src/components/Ticker/TickerInfo/TickerInfo.tsx
@@ -1,5 +1,22 @@
+/**
+ * Display
+ * - ticker name
+ * - last close price (also open price?)
+ * - last day % change
+ *  - interact to change to last week, month, etc.
+ * - industry
+ * - market cap
+ * - float
+ * - link to filings
+ * - link to news, finviz, ...anything else?
+ *
+ *
+ */
+
+import { StyledTickerInfo } from "./TickerInfo.style";
+
 const TickerInfo = (props) => {
-    return <div></div>;
+    return <StyledTickerInfo>{/*  */}</StyledTickerInfo>;
 };
 
 export default TickerInfo;
