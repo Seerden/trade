bbe1265fa9236e22f9f98f447fea7973e8f81722
Christiaan Seerden <christiaanseerden@live.nl>
2022-02-13 00:37:49 +0100

    (dev) remove all routes, only leave NewTicket for dev purposes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
 import axios from "axios";
-import Login from "components/Login/Login";
-import Navigation from "components/Navigation/Navigation";
-import Ticker from "components/Ticker/Ticker";
-import Tickers from "components/Ticker/Tickers";
-import UserPage from "components/User/UserPage";
+import NewTicket from "components/Ticker/Tickets/NewTicket/NewTicket";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import * as S from "./App.style";
+import { Wrapper } from "./App.style";
 
 axios.defaults.baseURL = "http://localhost:5000";
 
@@ -13,18 +9,12 @@ const App = () => {
     return (
         <div className="App">
             <BrowserRouter>
-                <Navigation />
-                <S.Wrapper>
+                {/* <Navigation /> */}
+                <Wrapper>
                     <Routes>
-                        <Route path="/" element={<Login />} />
-                        <Route path="u/:username" element={<UserPage />} />
-                        <Route path="tickets">
-                            <Route index element={<></>} />
-                        </Route>
-                        <Route path="ticker" element={<Tickers />} />
-                        <Route path="ticker/:tickerId" element={<Ticker />} />
+                        <Route path="/" element={<NewTicket />} />
                     </Routes>
-                </S.Wrapper>
+                </Wrapper>
             </BrowserRouter>
         </div>
     );
