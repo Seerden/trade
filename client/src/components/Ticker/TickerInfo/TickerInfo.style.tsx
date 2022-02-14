ba46a83a767efdae8b5dcb10a8a021c7c7ed78e0
Christiaan Seerden <christiaanseerden@live.nl>
2022-02-14 01:41:16 +0100

    remove TickerInfo component for now

--- client/src/components/Ticker/TickerInfo/TickerInfo.style.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const StyledTickerInfo = styled.div`
-    /* common section style here. 
-        share with NewTicket, TradeCard, etc.
-     */
-`;
