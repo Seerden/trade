07bc2dac423f3c85c6b38aaac23401d305295a62
Christiaan Seerden <christiaanseerden@live.nl>
2022-03-01 00:15:13 +0100

    (dev) add Test component in App.tsx to test API endpoint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,32 @@
 import axios from "axios";
-import NewTicket from "components/Ticker/Tickets/NewTicket/NewTicket";
+// import NewTicket from "components/Ticker/Tickets/NewTicket/NewTicket";
 import { theme } from "helpers/theme/theme";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import { Wrapper } from "./App.style";
+const CandleChart = lazy(() => import("./components/Charts/CandleChart/CandleChart"));
 
 axios.defaults.baseURL = "http://localhost:5000";
+
+function Test() {
+    const [data, setData] = useState<Record<string, any>>();
+    useEffect(() => {
+        (async (): Promise<void> => {
+            try {
+                const { data } = await axios.get("/msft/1m/2021-10-04/2021-10-05");
+                setData(data.rows.slice(350, 450));
+            } catch (error) {
+                console.error(error);
+            }
+        })();
+    }, []);
+
+    useEffect(() => {
+        console.log(data);
+    }, [data]);
+    return <></>;
+}
 
 const App = () => {
     return (
@@ -15,7 +36,15 @@ const App = () => {
                 <ThemeProvider theme={theme}>
                     <Wrapper>
                         <Routes>
-                            <Route path="/" element={<NewTicket />} />
+                            <Route
+                                path="/"
+                                element={
+                                    <Suspense fallback={<></>}>
+                                        <Test />
+                                        {/* <CandleChart /> */}
+                                    </Suspense>
+                                }
+                            />
                         </Routes>
                     </Wrapper>
                 </ThemeProvider>
