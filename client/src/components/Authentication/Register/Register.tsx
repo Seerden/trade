2244d2d003d5b01845e323c74233e945870acfa8
Christiaan Seerden <christiaanseerden@live.nl>
2022-03-27 17:02:04 +0200

    (Register) route to Login on 'sign in' click

--- client/src/components/Authentication/Register/Register.tsx
+++ client/src/components/Authentication/Register/Register.tsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import {
-    StyledButton,
-    StyledButtons,
-    StyledFields,
-    StyledForm,
-    StyledInput,
-    StyledLabel,
-    StyledTitle
+	StyledButton,
+	StyledButtons,
+	StyledFields,
+	StyledForm,
+	StyledInput,
+	StyledLabel,
+	StyledTitle
 } from "../Login/Login.style";
 
 /**
@@ -18,33 +19,35 @@ import {
 */
 
 export default function Register() {
-    return (
-        <StyledForm>
-            <StyledTitle>Register</StyledTitle>
-            <StyledFields>
-                <div>
-                    {/* username field */}
-                    <StyledLabel htmlFor={"username"}>Username</StyledLabel>
-                    <StyledInput type="text" name="username" />
-                </div>
+	return (
+		<StyledForm>
+			<StyledTitle>Register</StyledTitle>
+			<StyledFields>
+				<div>
+					{/* username field */}
+					<StyledLabel htmlFor={"username"}>Username</StyledLabel>
+					<StyledInput type="text" name="username" />
+				</div>
 
-                <div>
-                    {/* password field */}
-                    <StyledLabel htmlFor={"password"}>Password</StyledLabel>
-                    <StyledInput type="password" name="password" />
-                </div>
+				<div>
+					{/* password field */}
+					<StyledLabel htmlFor={"password"}>Password</StyledLabel>
+					<StyledInput type="password" name="password" />
+				</div>
 
-                <div>
-                    {/* repeat password field */}
-                    <StyledLabel htmlFor={"repeatPassword"}>Repeat password</StyledLabel>
-                    <StyledInput type="password" name="repeatPassword" />
-                </div>
-                <StyledButtons>
-                    <StyledButton type="submit" value="Register" />
-                    {/* @todo: rename isRegisterButton to isFaded or something */}
-                    <StyledButton type="button" value="Sign in" isRegisterButton />
-                </StyledButtons>
-            </StyledFields>
-        </StyledForm>
-    );
+				<div>
+					{/* repeat password field */}
+					<StyledLabel htmlFor={"repeatPassword"}>Repeat password</StyledLabel>
+					<StyledInput type="password" name="repeatPassword" />
+				</div>
+				<StyledButtons>
+					<StyledButton type="submit" value="Register" />
+					{/* @todo: rename isRegisterButton to isFaded or something */}
+					<Link to="/login">
+						<StyledButton type="button" value="Sign in" isRegisterButton />
+					</Link>
+				</StyledButtons>
+			</StyledFields>
+		</StyledForm>
+	);
 }
