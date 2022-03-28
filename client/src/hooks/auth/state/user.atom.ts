2d25d8b1bdf50faa083f687120f27ef3bd9cb78a
Christiaan Seerden <christiaanseerden@live.nl>
2022-03-29 00:47:21 +0200

    (user.atom) use nullish coalescing instead of OR operator for default userState object

--- client/src/hooks/auth/state/user.atom.ts
+++ client/src/hooks/auth/state/user.atom.ts
@@ -3,12 +3,12 @@ import { atom } from "recoil";
 
 // @todo: this should become the type for authenticated users - so no password, but perhaps creation date, role, etc.
 export type User = {
-    username: string;
+	username: string;
 };
 
 export const userState = atom<User>({
-    default: {
-        username: localStorageUser.get()?.username || ""
-    },
-    key: "auth/user"
+	default: {
+		username: localStorageUser.get()?.username ?? ""
+	},
+	key: "auth/user"
 });
