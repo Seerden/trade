df1e410c458da9f806f4a2fec0a4af2c85a102b1
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-01 00:08:56 +0200

    (tickets POST) update indentation, start writing a few helper functions to parse raw newTickets to database-ready arrays

--- server/api/database/queries/tickets/insert.ts
+++ server/api/database/queries/tickets/insert.ts
@@ -4,39 +4,90 @@ import { NewTicket } from "../../../types/ticket.types";
 import { getTradeIds } from "../trades/get";
 import { getUserId } from "../users/get";
 
+const fields = "ticker timestamp action quantity price".split(" ") as Array<
+	keyof NewTicket
+>;
+
+/**
+ * TODO: currently, addIdPropsToTicket and ticketObjectToArray are unused,
+ * we first need to
+ * - add a new type that extends NewTicket with userId and tradeId props (are
+ *   they numbers, or strings?)
+ * - change the `fields` variable (see above) to include userId and tradeId props
+ * - add trade_id property to the database insert
+ *  - actually, do we want to do this? think so
+ *
+ * Then we can use these two functions in the creation of ticketsAsArrays in insertTickets.
+ * Note the order of the fields we insert into the database.
+ *
+ */
+
+/**
+ * Take a raw newTicket object, e.g. as expected to be sent from the client,
+ * and add properties `userId` and `tradeId` to it
+ */
+function addIdPropsToTicket(ticket: NewTicket, userId: number, tradeId: number) {
+	return { ...ticket, userId, tradeId };
+}
+
+/**
+ * Take a newTicket object like {
+ *    ticker,
+ *    timestamp,
+ *    action,
+ *    quantity,
+ *    price
+ * }
+ *
+ * and convert it to an array like [
+ *    ticker,
+ *    timestamp,
+ *    action,
+ *    quantity,
+ *    price
+ * ]
+ */
+function ticketObjectToArray(ticket: NewTicket) {
+	const array = [];
+
+	for (const field of fields) {
+		if (field in ticket) {
+			array.push(ticket[field]);
+		}
+	}
+
+	return array;
+}
+
 /**
  * Insert a list of new tickets into the database
  */
 export async function insertTickets(username: string, tickets: Array<NewTicket>) {
-    /**
-     * create a list that's usable with pg-format
-     *
-     * so go
-     *  from    Array<Ticket>
-     *  to      Array<[user_id, ticker, timestamp, action, ...]>
-     */
+	/**
+	 * create a list that's usable with pg-format
+	 *
+	 * so go
+	 *  from    Array<Ticket>
+	 *  to      Array<[user_id, ticker, timestamp, action, ...]>
+	 */
 
-    const fields = "ticker, timestamp, action, quantity, price".split(", ");
-    const userId = await getUserId(username);
-    const tradeIds = await getTradeIds(tickets);
-    const ticketsAsArrays = tickets.map((ticket, index) =>
-        fields.reduce(
-            (
-                acc: Array<NewTicket[keyof NewTicket]>,
-                field: NewTicket[keyof NewTicket]
-            ) => {
-                // @ts-ignore - reducing into a array with variable types is.. hacky
-                acc.push(ticket[field]);
-                return acc;
-            },
-            // userId isn't part of the newTickets array passed from the
-            // frontend, so add it to `acc` manually
-            [userId, tradeIds[index]]
-        )
-    );
+	const userId = await getUserId(username);
+	const tradeIds = await getTradeIds(tickets);
+	const ticketsAsArrays = tickets.map((ticket, index) =>
+		fields.reduce(
+			(acc: Array<NewTicket[keyof NewTicket]>, field: NewTicket[keyof NewTicket]) => {
+				// @ts-ignore - reducing into a array with variable types is.. hacky
+				acc.push(ticket[field]);
+				return acc;
+			},
+			// userId isn't part of the newTickets array passed from the
+			// frontend, so add it to `acc` manually
+			[userId, tradeIds[index]]
+		)
+	);
 
-    API.query({
-        text: format(`
+	API.query({
+		text: format(`
             insert into tickets (
                 user_id, -- @todo: instead of typing out the fields, re-use the 'fields' variable from above
                 ticker, 
@@ -46,6 +97,6 @@ export async function insertTickets(username: string, tickets: Array<NewTicket>)
                 price
             ) values %L
         `),
-        values: ticketsAsArrays,
-    });
+		values: ticketsAsArrays,
+	});
 }
