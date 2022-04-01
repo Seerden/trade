d68382528e5ca17604fe4710dad2d40359518e22
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-02 00:54:10 +0200

    (dev-router) add /latest trades endpoint for testing, run prettier

--- server/api/routers/dev-router.ts
+++ server/api/routers/dev-router.ts
@@ -5,13 +5,14 @@ import utc from "dayjs/plugin/utc";
 import express from "express";
 import { fetchPriceActionForTicker } from "../../price-action/database/queries/fetch-price-action";
 import {
-    // eslint-disable-next-line camelcase
-    dev_firstAndLastMaxOneMinuteDataResultRow,
-    fetchAndInsertMaxOneMinuteData,
-    fetchMaxOneMinuteData,
+	// eslint-disable-next-line camelcase
+	dev_firstAndLastMaxOneMinuteDataResultRow,
+	fetchAndInsertMaxOneMinuteData,
+	fetchMaxOneMinuteData,
 } from "../../price-action/database/_dev/polygon/max-1m-query";
 import { fetchDailyOHLC } from "../../price-action/lib/polygon/requests/snapshot/snapshot-fetch";
 import { fetchAndInsertSnapshot } from "../../price-action/lib/polygon/requests/snapshot/snapshot-insert";
+import { getLatestTrade } from "../database/queries/trades/get";
 
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
@@ -23,48 +24,60 @@ dayjs.extend(timezone);
 export const devRouter = express.Router({ mergeParams: true });
 
 devRouter.get("/daily/all", async (req, res) => {
-    const response = await fetchDailyOHLC({ date: "2021-12-13" });
-    res.json({ response });
+	const response = await fetchDailyOHLC({ date: "2021-12-13" });
+	res.json({ response });
 });
 
 devRouter.post("/:ticker/1m/:to", async (req, res) => {
-    const { ticker, to } = req.params;
+	const { ticker, to } = req.params;
 
-    res.json({
-        ranges: await fetchAndInsertMaxOneMinuteData({
-            ticker,
-            to,
-        }),
-    });
+	res.json({
+		ranges: await fetchAndInsertMaxOneMinuteData({
+			ticker,
+			to,
+		}),
+	});
 });
 
 devRouter.get("/:ticker/1m/max/:to", async (req, res) => {
-    const { ticker, to } = req.params;
+	const { ticker, to } = req.params;
 
-    const response = await fetchMaxOneMinuteData({ ticker, to });
+	const response = await fetchMaxOneMinuteData({ ticker, to });
 
-    res.json({ resultEndpoints: dev_firstAndLastMaxOneMinuteDataResultRow(response) });
+	res.json({ resultEndpoints: dev_firstAndLastMaxOneMinuteDataResultRow(response) });
 });
 
 devRouter.get("/:ticker/1m/:from/:to", async (req, res) => {
-    const { ticker, from, to } = req.params;
-    const { limit } = req.query;
+	const { ticker, from, to } = req.params;
+	const { limit } = req.query;
 
-    const rows = await fetchPriceActionForTicker({
-        timescale: "minute",
-        ticker: ticker.toUpperCase(),
-        from,
-        to,
-        limit: limit ? String(limit) : null,
-    });
+	const rows = await fetchPriceActionForTicker({
+		timescale: "minute",
+		ticker: ticker.toUpperCase(),
+		from,
+		to,
+		limit: limit ? String(limit) : null,
+	});
 
-    res.json({ rows });
+	res.json({ rows });
 });
 
 /**
  * Fetch and store in our database a market snapshot from a given day
  */
 devRouter.post("/snapshot/:date", async (req, res) => {
-    const { date } = req.params;
-    res.json({ timestamp: await fetchAndInsertSnapshot({ date }) });
+	const { date } = req.params;
+	res.json({ timestamp: await fetchAndInsertSnapshot({ date }) });
+});
+
+devRouter.get("/latest", async (req, res) => {
+	const response = await getLatestTrade({
+		userId: 1,
+		tickers: ["msft", "aapl"],
+	});
+
+	res.json({
+		test: "hey",
+		response,
+	});
 });
