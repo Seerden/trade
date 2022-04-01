c77ad2f1a1d6215643e47a3453cb37e8b517a0ab
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-01 23:14:38 +0200

    (formatting) run prettier on index.ts

--- server/index.ts
+++ server/index.ts
@@ -12,72 +12,72 @@ import { redisSession, startRedis } from "./store/redis-client";
 config();
 
 function logRequests(req: Request, res: Response, next: (...args: any) => any) {
-    const { method, url, body, query } = req;
-    console.log({
-        method,
-        url,
-        body,
-        query,
-    });
-    next();
+	const { method, url, body, query } = req;
+	console.log({
+		method,
+		url,
+		body,
+		query,
+	});
+	next();
 }
 
 async function main() {
-    const app = express();
-    app.use(
-        express.urlencoded({
-            limit: "5mb",
-            parameterLimit: 100,
-            extended: true,
-        })
-    );
-    app.use(express.json());
-    app.use(logRequests);
-    app.use(
-        cors({
-            // @todo: origin should probably be different in production
-            origin: "http://localhost:3000",
-            methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
-            credentials: true,
-        })
-    );
+	const app = express();
+	app.use(
+		express.urlencoded({
+			limit: "5mb",
+			parameterLimit: 100,
+			extended: true,
+		})
+	);
+	app.use(express.json());
+	app.use(logRequests);
+	app.use(
+		cors({
+			// @todo: origin should probably be different in production
+			origin: "http://localhost:3000",
+			methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
+			credentials: true,
+		})
+	);
 
-    await startRedis();
-    app.use(session(redisSession));
+	await startRedis();
+	app.use(session(redisSession));
 
-    passport.use(strategy);
-    app.use(passport.initialize());
-    app.use(passport.session());
+	passport.use(strategy);
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-    passport.serializeUser((user: any, done) => done(null, user));
+	passport.serializeUser((user: any, done) => done(null, user));
 
-    passport.deserializeUser(async (user: { username: string }, done) => {
-        try {
-            const [foundUser] = await getUser(user.username);
-            if ("username" in foundUser) {
-                return done(null, foundUser);
-            }
-            return done("User not found");
-        } catch (e) {
-            done(e);
-        }
-    });
+	passport.deserializeUser(async (user: { username: string }, done) => {
+		try {
+			const [foundUser] = await getUser(user.username);
+			if ("username" in foundUser) {
+				return done(null, foundUser);
+			}
+			return done("User not found");
+		} catch (e) {
+			done(e);
+		}
+	});
 
-    if (!(process.env.NODE_ENV === "production")) {
-        const { devRouter } = await import("./api/routers/dev-router");
-        app.use("/", devRouter);
-    }
+	if (!(process.env.NODE_ENV === "production")) {
+		const { devRouter } = await import("./api/routers/dev-router");
+		app.use("/", devRouter);
+	}
 
-    app.use("/auth", authRouter);
-    app.use("/t", tradeRouter);
+	app.use("/auth", authRouter);
+	app.use("/t", tradeRouter);
 
-    app.get("/", (req, res) => {
-        res.json({ message: "/ GET successful" });
-    });
+	app.get("/", (req, res) => {
+		res.json({ message: "/ GET successful" });
+	});
 
-    app.listen(process.env.PORT || 5000, () => {
-        console.log(`Server started on ${new Date()}`);
-    });
+	app.listen(process.env.PORT || 5000, () => {
+		console.log(`Server started on ${new Date()}`);
+	});
 }
 
 main().catch((e) => console.error(e));
