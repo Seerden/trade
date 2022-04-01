50c4aae8837e30e3303d5e95079d7d3d3759413e
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-02 00:53:42 +0200

    (getLatestTrade) join trade result with tickets for that trade, update return type: this breaks usage of this function

--- server/api/database/queries/trades/get.ts
+++ server/api/database/queries/trades/get.ts
@@ -24,23 +24,48 @@ export async function getLatestTrade({
 	userId: number;
 	tickers: string[];
 }) {
-	const { rows } = await BackendApiObject.query({
-		text: format(
+	try {
+		const text = format(
 			`
-            select * from (
+            select to_json(sub.*) "trade", to_json(tickets.*) "ticket" from (
                select t.*, rank() over (
                   partition by t.ticker
                   order by t.trade_id desc
                ) rank_number 
                from trades t 
-               where t.ticker in %L
-               and t.user_id = %I
+               where t.ticker in (%L)
+               and t.user_id = %L
             ) sub
-            where sub.rank_number = 1
+            inner join tickets
+            on tickets.trade_id = sub.trade_id
+            and sub.rank_number = 1
          `,
-			[tickers, userId]
-		),
-	});
+			tickers,
+			userId
+		);
 
-	return rows;
+		const response = await BackendApiObject.query({ text });
+
+		return response as {
+			trade: {
+				user_id: number;
+				trade_id: number;
+				ticker: string;
+				trade_type: string;
+				rank_number: string;
+			};
+			ticket: {
+				user_id: number;
+				trade_id: number;
+				ticket_id: number;
+				ticker: string;
+				timestamp: number;
+				action: string;
+				quantity: number;
+				price: number;
+			};
+		}[];
+	} catch (e) {
+		console.error(e);
+	}
 }
