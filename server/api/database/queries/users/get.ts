f4f2f23b4fc252557fbc79b4991ae5c098e83223
Christiaan Seerden <c@seerden.io>
2022-04-01 16:15:36 +0200

    (db) add comment to users/get file

--- server/api/database/queries/users/get.ts
+++ server/api/database/queries/users/get.ts
@@ -1,8 +1,14 @@
 import { BackendApiObject as API } from "../../../../database/pools/query-objects";
 
+/**
+ * TODO: the users table is simple enough that it might be better to, for now,
+ * have one fetchUser() transaction function, and then a few helpers to extract
+ * only the things we need, instead of separate transactions like getUsername(), getUserId()
+ */
+
 export async function getUserId(username: string) {
-    return await API.query({
-        text: "select user_id from users where username = $1",
-        values: [username],
-    });
+	return await API.query({
+		text: "select user_id from users where username = $1",
+		values: [username],
+	});
 }
