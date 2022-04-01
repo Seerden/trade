1b74893953e9193a8c42041cb9990d004e46a3d1
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-01 23:44:02 +0200

    (trades/insert) update TSDoc for insertTradeRow

--- server/api/database/queries/trades/insert.ts
+++ server/api/database/queries/trades/insert.ts
@@ -6,17 +6,10 @@ type Options = {
 	tradeType: "long" | "short";
 };
 
-/*
-    Insert trade rows into the database.
-    trade has 
-    user_id: number (foreign key on `users`) 
-    trade_id: number (auto-incremented sequentially generated int), 
-    ticker: string (like MSFT - undecided if we want to store them in uppercase
-    or lowercase),
-    trade_type: long/short
-
-*/
-
+/**
+ * Insert a trade row into the database.
+ * @todo: do we want to cast ticker to uppercase?
+ */
 export async function insertTradeRow({ userId, ticker, tradeType }: Options) {
 	return await API.query({
 		text: "insert into trades(user_id, ticker, trade_type) values($1, $2, $3)",
