559c0d6b51111f0d562ce42d7389c7413547398a
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-02 23:50:46 +0200

    (NewTicket/helpers/date) add toNewYorkTime function from backend

---
+++ client/src/components/NewTicket/NewTickets/helpers/date.ts
@@ -0,0 +1,18 @@
+import dayjs, { Dayjs } from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+type DateDayjsOrString = Date | Dayjs | string;
+
+/**
+ * Take a date and convert it to US market time (i.e. New York local time) as a string
+ * To get something usable as New York time, we have to convert to a string, otherwis dayjs will,
+ * with most methods, end up using the host machine's locale again.
+ */
+export function toNewYorkTime(date: DateDayjsOrString, format = "YYYY-MM-DD HH:mm:ss") {
+	return dayjs(date)
+		.tz("America/New_York")
+		.format(format);
+}
