359d2769c9cdfc951b3f68917ad0c8d4854c077a
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-03 20:08:46 +0200

    (style) change header font style and change 'Time' label to 'Market Time

--- client/src/components/NewTicket/NewTickets/sub/Header.tsx
+++ client/src/components/NewTicket/NewTickets/sub/Header.tsx
@@ -2,12 +2,15 @@ import styled from "styled-components";
 
 export default function Header() {
 	const columns = "Action Ticker Price Quantity Date Time".split(" ");
+	const columnMap = {
+		Time: "Market Time"
+	};
 
 	return (
 		<StyledHeader>
 			{columns.map((col, index) => (
 				<label htmlFor={col.toLowerCase()} key={index}>
-					{col}
+					{columnMap[col] ?? col}
 				</label>
 			))}
 		</StyledHeader>
@@ -17,12 +20,13 @@ export default function Header() {
 const StyledHeader = styled.div`
 	display: grid;
 	grid-template-columns: repeat(4, 5rem) 10rem 5.5rem;
-	gap: 0;
+	gap: 0.45rem;
 
 	label {
 		user-select: none;
 		text-align: center;
-		font-size: 0.82rem;
+		font-size: 0.88rem;
+		font-weight: 500;
 		border-bottom: 2px solid #eee;
 		padding-bottom: 0.25rem;
 		margin-bottom: 0.3rem;
