a3222370b75e16a16a177a5a4b367c6441396868
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-04 00:30:05 +0200

    (user/get) return user_id instead of password

--- server/api/helpers/auth/user.ts
+++ server/api/helpers/auth/user.ts
@@ -5,27 +5,27 @@ import { BackendApiObject as API } from "../../../database/pools/query-objects";
  * Fetch user from database, by username
  */
 export async function getUser(username: string) {
-    return await API.query({
-        text: "select username, created_at, password from users where username = $1",
-        values: [username],
-    });
+	return await API.query({
+		text: "select username, created_at, user_id from users where username = $1",
+		values: [username],
+	});
 }
 
 type NewUser = {
-    username: string;
-    password: string;
+	username: string;
+	password: string;
 };
 
 export async function createUser({ username, password }: NewUser) {
-    const hashedPassword = await hash(password, 10);
+	const hashedPassword = await hash(password, 10);
 
-    // TODO: what happens if we try to insert a user with a username that's
-    // already taken?
-    return await API.query({
-        text: `
+	// TODO: what happens if we try to insert a user with a username that's
+	// already taken?
+	return await API.query({
+		text: `
             insert into users (username, password) values ($1, $2) 
             returning username, user_id, created_at
         `,
-        values: [username, hashedPassword],
-    });
+		values: [username, hashedPassword],
+	});
 }
