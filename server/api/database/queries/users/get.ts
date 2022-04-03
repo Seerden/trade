a0efce0fd5dea458720af526a02dbb9627500c68
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-03 15:22:38 +0200

    (user/get) only return user_id with proper type

--- server/api/database/queries/users/get.ts
+++ server/api/database/queries/users/get.ts
@@ -1,4 +1,6 @@
 import { BackendApiObject as API } from "../../../../database/pools/query-objects";
+import { User } from "../../../../types/database.types";
+import { Maybe } from "../../../../types/global";
 
 /**
  * TODO: the users table is simple enough that it might be better to, for now,
@@ -7,8 +9,10 @@ import { BackendApiObject as API } from "../../../../database/pools/query-object
  */
 
 export async function getUserId(username: string) {
-	return await API.query({
+	const [response] = (await API.query({
 		text: "select user_id from users where username = $1",
 		values: [username],
-	});
+	})) as [Maybe<User>];
+
+	return response?.user_id ?? null;
 }
