4aef52e263e1bfa578a17c3d32db9105b40b76c5
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-04 00:39:05 +0200

    (GET auth/me) only return username

--- server/api/routers/auth-router.ts
+++ server/api/routers/auth-router.ts
@@ -6,48 +6,49 @@ import { createUser, getUser } from "../helpers/auth/user";
 const authRouter = express.Router({ mergeParams: true });
 
 authRouter.post("/register", async (req, res) => {
-    const { username, password } = req.body.newUser;
+	const { username, password } = req.body.newUser;
 
-    const [existingUser] = await getUser(username);
+	const [existingUser] = await getUser(username);
 
-    if (existingUser) {
-        res.status(403).json({
-            message: "Username already exists",
-        });
-    } else {
-        // TODO: log user in after inserting `user` row into database
-        const [newUser] = await createUser({ username, password });
-        res.json({ newUser });
-    }
+	if (existingUser) {
+		res.status(403).json({
+			message: "Username already exists",
+		});
+	} else {
+		// TODO: log user in after inserting `user` row into database
+		const [newUser] = await createUser({ username, password });
+		res.json({ newUser });
+	}
 });
 
 authRouter.post("/login", passport.authenticate("local"), (req, res) => {
-    const { username, created_at } = req.user as any;
-    const userFieldsForClient = {
-        // @todo feat/auth: this should become a type shared with frontend so we can use it as axios response type
-        username,
-        createdAt: created_at,
-    };
+	const { username, created_at } = req.user as any;
+	const userFieldsForClient = {
+		// @todo feat/auth: this should become a type shared with frontend so we can use it as axios response type
+		username,
+		createdAt: created_at,
+	};
 
-    res.json({ ...userFieldsForClient });
+	res.json({ ...userFieldsForClient });
 });
 
 authRouter.get("/me", (req, res) => {
-    if (req.isAuthenticated() && req.user) {
-        res.send(req.user);
-    } else {
-        res.status(401).send("Not authenticated");
-    }
+	if (req.isAuthenticated() && req.user) {
+		const { username } = req.user as any;
+		res.json({ username });
+	} else {
+		res.status(401).send("Not authenticated");
+	}
 });
 
 authRouter.post("/logout", (req, res) => {
-    req.session.destroy(() => {
-        req.logOut();
-        res.json({
-            success: true,
-            message: "Logged out successfully.",
-        });
-    });
+	req.session.destroy(() => {
+		req.logOut();
+		res.json({
+			success: true,
+			message: "Logged out successfully.",
+		});
+	});
 });
 
 export default authRouter;
