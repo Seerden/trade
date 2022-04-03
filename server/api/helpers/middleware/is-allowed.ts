0d9ddeceff297538f91146c5486f14b883d89183
Christiaan Seerden <48338210+Seerden@users.noreply.github.com>
2022-04-03 22:21:06 +0200

    Middleware: add isAllowed middleware (#17)
    * (server, types) add User type
    
    * (server/index) do not use user: any type in passport.serialize
    
    * write isAllowed middleware function and use it in tradeRouter
    
    * move logRequests middleware to helpers/middleware folder

---
+++ server/api/helpers/middleware/is-allowed.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+
+export function isAllowed(req: Request, res: Response, next: (args?: any) => any) {
+	// @ts-ignore
+	if (!req.user?.username || !req.body?.username) {
+		res.json({
+			message:
+				"Authenticated request, but at least one of req.user.username and req.body.username does not exist.",
+		});
+	}
+
+	// @ts-ignore
+	if (req.user.username === req.body.username) {
+		next?.();
+	} else {
+		res.json({
+			message:
+				"User may only make a request for their own data; provided username does not match logged in user.",
+		});
+	}
+}
