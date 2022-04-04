0192821ca7a4afc9b9437b5d15a18df5b2d41060
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-04 17:01:27 +0200

    (server/axios-instance) run prettier

--- server/price-action/lib/polygon/axios-instance.ts
+++ server/price-action/lib/polygon/axios-instance.ts
@@ -1,15 +1,16 @@
 import axios from "axios";
 import { config } from "dotenv";
+
 config();
 
 const { POLYGON_KEY } = process.env;
 
 // create an axios instance with authentication header bearer token
 const axiosInstance = axios.create({
-    baseURL: "https://api.polygon.io",
-    headers: {
-        Authorization: `Bearer ${POLYGON_KEY}`,
-    },
+	baseURL: "https://api.polygon.io",
+	headers: {
+		Authorization: `Bearer ${POLYGON_KEY}`,
+	},
 });
 
 export default axiosInstance;
