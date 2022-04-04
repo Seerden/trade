345c9672f07902e4567bc968c9b6f0e1806bbf08
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-05 00:03:22 +0200

    (style) move NewTicket and NewTickets styles to .style.tsx files

--- client/src/components/NewTicket/NewTickets/NewTicket.tsx
+++ client/src/components/NewTicket/NewTickets/NewTicket.tsx
@@ -1,6 +1,6 @@
 import { useMemo } from "react";
 import { BsX } from "react-icons/bs";
-import styled, { css } from "styled-components";
+import { StyledButton, StyledNewTicket } from "./NewTicket.style";
 import Input from "./sub/Input";
 import TradeActionButton from "./sub/TradeActionButton";
 import { useNewTickets } from "./useNewTickets";
@@ -15,26 +15,6 @@ export type RawNewTicket = {
 	time: string;
 	price: string;
 };
-
-const StyledNewTicket = styled.fieldset<{ empty?: boolean }>`
-	${p =>
-		p.empty &&
-		css`
-			transform: translateZ(0);
-			&:not(:hover) {
-				opacity: 0.45;
-				filter: grayscale(1);
-			}
-		`}
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	height: 2.5rem;
-	gap: 0.45rem;
-	font-size: 0.88rem;
-
-	transition: filter 75ms linear, opacity 90ms linear;
-`;
 
 type Props = {
 	ticketIndex: number;
@@ -184,27 +164,3 @@ const NewTicket = ({ ticketIndex, ticket, setAction, setField, deleteTicket }: P
 };
 
 export default NewTicket;
-
-const StyledButton = styled.button`
-	display: inline-flex;
-
-	align-items: center;
-	justify-content: center;
-	background-color: transparent;
-
-	width: 25px;
-	min-height: 25px;
-	height: 25px;
-	line-height: 100%;
-
-	border-radius: 50%;
-
-	transition: color 35ms ease-in, transform 50ms ease-out;
-
-	&:hover,
-	&:active {
-		color: red;
-		font-weight: 500;
-		transform: scale(1.4);
-	}
-`;
--- client/src/components/NewTicket/NewTickets/NewTickets.tsx
+++ client/src/components/NewTicket/NewTickets/NewTickets.tsx
@@ -1,61 +1,16 @@
 import { useMemo } from "react";
-import styled, { css } from "styled-components";
 import NewTicket from "./NewTicket";
+import {
+	StyledButton,
+	StyledButtons,
+	StyledButtonWrapper,
+	StyledNewTickets,
+	StyledSubtitle,
+	StyledTickets,
+	StyledTitle
+} from "./NewTickets.style";
 import Header from "./sub/Header";
 import { useNewTickets } from "./useNewTickets";
-
-const StyledNewTickets = styled.form`
-	display: block;
-	margin: 0 auto;
-	max-width: max-content;
-	width: max-content;
-
-	padding: 2rem 4rem;
-	border-radius: 5px;
-	box-shadow: 0 0 0.2rem 0 #ccc;
-`;
-
-const StyledTitle = styled.h1`
-	display: block;
-	width: max-content;
-	position: relative;
-	z-index: 1;
-	font-size: 1.4rem;
-	font-weight: 600;
-	user-select: none;
-	margin-bottom: 0.5rem;
-
-	&::after {
-		content: "";
-		display: block;
-		min-height: 15px;
-		margin-top: -10px;
-		margin-left: -15px;
-		box-shadow: -3px 3px 0 0 #eee;
-		width: 200%;
-		position: static;
-		z-index: 0;
-		top: 1rem;
-		left: 1rem;
-		background-color: #fefefe;
-	}
-`;
-
-const StyledSubtitle = styled.h3`
-	font-size: 0.9rem;
-	word-wrap: break-word;
-	color: #aaa;
-	font-weight: 350;
-
-	margin: 0 auto;
-
-	max-width: 720px;
-	margin-bottom: 0.8rem;
-`;
-
-const StyledTickets = styled.section`
-	margin: 2.1rem 0;
-`;
 
 /**
    Form to create new tickets. Acts like one large form, but UX should feel like a Google
@@ -110,57 +65,6 @@ export default function NewTickets() {
 		</>
 	);
 }
-
-const StyledButtons = styled.span`
-	margin-bottom: 0.8rem;
-	display: block;
-	position: sticky;
-	z-index: 500;
-	top: 1rem;
-	flex-direction: row;
-
-	gap: 0.4rem;
-`;
-
-const StyledButton = styled.input<{ round?: boolean }>`
-	${p =>
-		p.round
-			? css`
-					width: 25px;
-					height: 25px;
-					border-radius: 50%;
-					justify-content: center;
-					text-align: center;
-					display: inline-block;
-					vertical-align: middle;
-			  `
-			: css`
-					width: max-content;
-					padding: 0.3rem 1.6rem;
-			  `}
-	display: inline-flex;
-	background-color: transparent;
-	font-size: 0.72rem;
-
-	border: 2px solid ${p => p.theme.colors.grey.light};
-
-	transition: all 75ms ease-out;
-	&:hover {
-		box-shadow: 0 4px 0 -2px lightgrey;
-		transform: translateY(-2px);
-	}
-`;
-
-const StyledButtonWrapper = styled.span`
-	display: flex;
-	justify-content: space-between;
-
-	span {
-		display: inline-flex;
-		align-items: center;
-		gap: 0.4rem;
-	}
-`;
 
 function Buttons({ addTicketRows }: { addTicketRows: (_: number) => void }) {
 	return (
