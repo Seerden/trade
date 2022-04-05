a27092547a29efef505caa4e3d0f8c49c5e4f0a5
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-06 00:33:14 +0200

    create Trades.tsx and write up small outline of intent

---
+++ client/src/components/Trades/Trades.tsx
@@ -0,0 +1,30 @@
+export default function Trades() {
+	// This component will be part of the (future) Ticker page, and probably also
+	// end up in various other places, like user profiles and P&L journals
+}
+
+function Trade() {
+	/**
+      a trade card contains the following:
+      
+      - duration
+      - realized gain
+      - unrealized gain (is only really a thing if we have access to a live data
+      source)
+      - fees
+      - average entry price
+      - average exit price
+      - open quantity
+      - total quantity
+      - ticker (with link to /ticker/:ticker page if not already on that page?)
+
+      - the design of this component may depend on what options we allow for
+        sorting
+         - are trades always sorted by ticker? day? type (long/short)? 
+
+
+
+    */
+
+	return <></>;
+}
