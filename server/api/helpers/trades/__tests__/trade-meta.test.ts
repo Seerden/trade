ab37b5fc6937624e4d9a4d29755a81b68f77d749
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-06 21:45:07 +0200

    (trade-meta, test) write unit tests for getRealizedProfit function

--- server/api/helpers/trades/__tests__/trade-meta.test.ts
+++ server/api/helpers/trades/__tests__/trade-meta.test.ts
@@ -1,4 +1,10 @@
-import { getAverage, getOpenQuantity, getTimestampRange, Tickets } from "../trade-meta";
+import {
+	getAverage,
+	getOpenQuantity,
+	getRealizedProfit,
+	getTimestampRange,
+	Tickets,
+} from "../trade-meta";
 
 const snippet = {
 	price: 100,
@@ -146,5 +152,46 @@ describe("getAverage", () => {
 			buy: 1.5,
 			sell: 1,
 		});
+	});
+});
+
+describe("getRealizedProfit", () => {
+	const tickets: Tickets = [
+		{
+			...snippet,
+			action: "buy",
+			quantity: 50,
+			price: 1,
+			ticket_id: 1,
+			timestamp: 1,
+		},
+		{
+			...snippet,
+			action: "sell",
+			quantity: 50,
+			price: 2,
+			ticket_id: 2,
+			timestamp: 2,
+		},
+	];
+
+	test("returns correct P&L for a trade with at least one buy and one sell ticket", () => {
+		const result = getRealizedProfit(tickets);
+		expect(result).toBe(50);
+	});
+
+	test("returns correct profit if multiple entries and exits", () => {
+		expect(getRealizedProfit([...tickets, ...tickets])).toBe(100);
+	});
+
+	test("returns correct profit (0) if 0 shares bought or 0 shares sold", () => {
+		tickets[0].quantity = 0;
+		expect(getRealizedProfit(tickets)).toBe(0);
+	});
+
+	test("returns correct profit if trade not complete", () => {
+		tickets[0].quantity = 50;
+		tickets.push({ ...tickets[0], price: 100, timestamp: 3 });
+		expect(getRealizedProfit(tickets)).toBe(50);
 	});
 });
