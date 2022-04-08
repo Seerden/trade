24555b9b5d1dddd1bf5a82b9ab49367715a378c5
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-08 23:43:22 +0200

    (feat/sentry) implement useSentry hook that initializes Sentry with v6 routing instrumentation

---
+++ client/src/hooks/useSentry.tsx
@@ -0,0 +1,17 @@
+import { init } from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+import { useEffect } from "react";
+import useRoutingInstrumentation from "react-router-v6-instrumentation";
+
+export default function useSentry() {
+	const routingInstrumentation = useRoutingInstrumentation();
+	useEffect(() => {
+		init({
+			dsn: process.env.SENTRY_DSN,
+			release: process.env.SENTRY_RELEASE,
+			integrations: [new BrowserTracing({ routingInstrumentation })],
+			tracesSampleRate: 1,
+			autoSessionTracking: false,
+		});
+	}, [routingInstrumentation]);
+}
