0d853f27d89a128e84c404aebc574a1e70d32140
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-10 00:33:46 +0200

    (wip/style/TicketSummary) adjust modal z-index and force StyledContainer to be centeredand fixed

--- client/src/components/NewTicket/NewTickets/TicketSummary.style.tsx
+++ client/src/components/NewTicket/NewTickets/TicketSummary.style.tsx
@@ -5,11 +5,12 @@ import styled from "styled-components";
  * theme value.
  */
 export const StyledContainer = styled.section`
-	position: relative;
-	z-index: 10;
+	position: fixed;
+	z-index: 21;
 
-	margin: 0 auto;
-	margin-top: 25%;
+	margin-top: 20vh;
+	left: 50%;
+	transform: translateX(-50%);
 	padding: 2rem;
 	border: 4px solid #444;
 	box-shadow: 0 0 1rem #111;
@@ -21,6 +22,7 @@ export const StyledContainer = styled.section`
 // This is the background that serves to add visual focus to the modal.
 export const StyledOverlay = styled.div`
 	position: fixed;
+	z-index: 20;
 	top: 0;
 	left: 0;
 	width: 100%;
