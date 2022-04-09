00ff7443c3451d72d314e680d1e6b24ff59d423f
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-09 23:15:39 +0200

    (feat/component) add TicketSummary component file

---
+++ client/src/components/NewTicket/NewTickets/TicketSummary.tsx
@@ -0,0 +1,10 @@
+/**
+ * This component encompasses two steps of the NewTicket(s) creation process:
+ * - user clicks "preview tickets" => preview all tickets so the user can verify
+ *   their input.
+ * - if in preview screen and user clicks "save tickets", make POST request and,
+ *   based on response, highlight the tickets from preview screen that were
+ *   saved to the database successfully
+ */
+
+export default function TicketSummary() {}
