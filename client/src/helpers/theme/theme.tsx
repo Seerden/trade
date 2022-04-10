207bef12c0f07948ba5d98da264a5eb387d788dc
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-11 00:35:43 +0200

    (style/theme) add font size values and a text color value

--- client/src/helpers/theme/theme.tsx
+++ client/src/helpers/theme/theme.tsx
@@ -7,6 +7,9 @@ export const colors = {
 		deep: "deepskyblue",
 		regular: "#99daff",
 	},
+	text: {
+		secondary: "#e3e3e3",
+	},
 };
 
 export const size = {
@@ -24,8 +27,20 @@ export const padding = {
 	large: "2.5rem",
 };
 
+export const font = {
+	tiny: "0.75rem",
+	smaller: "0.82rem",
+	small: "0.88rem",
+	medium: "0.92rem",
+	regular: "0.95rem",
+	large: "1.3rem",
+	larger: "1.55rem",
+	huge: "1.85rem",
+};
+
 export const theme = {
 	colors,
 	padding,
 	size,
+	font,
 };
