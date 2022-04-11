03bfc9ab9c111f6b43edb61db1769578b72c3472
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-11 23:31:08 +0200

    (storybook) install storybook react template

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,56 +1,50 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended",
-	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: "module",
-	},
-	plugins: ["react", "@typescript-eslint"],
-	settings: {
-		react: {
-			version: "detect",
-		},
-		"import/resolver": {
-			alias: {
-				map: [
-					["components", "src/components"],
-					["hooks", "src/hooks"],
-				],
-			},
-		},
-	},
-	rules: {
-		"max-len": 0,
-		"react/react-in-jsx-scope": "off",
-		"react/jsx-first-prop-new-line": [1, "multiline"],
-		"react/jsx-max-props-per-line": [1, { maximum: 1, when: "multiline" }],
-		"react/jsx-closing-bracket-location": [2, "tag-aligned"],
-		"react/prop-types": "off",
-		"react/display-name": "off",
-		"no-mixed-spaces-and-tabs": "off",
-		"react/no-unescaped-entities": "off",
-	},
-	overrides: [
-		{
-			files: ["**/*.js?(x)", "**/*.ts?(x)"],
-			rules: {
-				"react-hooks/exhaustive-deps": "off",
-				"@typescript-eslint/no-unused-vars": "off",
-				"@typescript-eslint/explicit-module-boundary-types": "off",
-				"no-unused-vars": "off",
-			},
-		},
-	],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:@typescript-eslint/recommended", "plugin:storybook/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect"
+    },
+    "import/resolver": {
+      alias: {
+        map: [["components", "src/components"], ["hooks", "src/hooks"]]
+      }
+    }
+  },
+  rules: {
+    "max-len": 0,
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-first-prop-new-line": [1, "multiline"],
+    "react/jsx-max-props-per-line": [1, {
+      maximum: 1,
+      when: "multiline"
+    }],
+    "react/jsx-closing-bracket-location": [2, "tag-aligned"],
+    "react/prop-types": "off",
+    "react/display-name": "off",
+    "no-mixed-spaces-and-tabs": "off",
+    "react/no-unescaped-entities": "off"
+  },
+  overrides: [{
+    files: ["**/*.js?(x)", "**/*.ts?(x)"],
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "no-unused-vars": "off"
+    }
+  }]
 };
