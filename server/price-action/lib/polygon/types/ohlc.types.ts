a8370fb4f821fe0f80e3b5d912ca5edd85f9715e
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-12 17:52:11 +0200

    (code style:ohlc.types) run prettier

--- server/price-action/lib/polygon/types/ohlc.types.ts
+++ server/price-action/lib/polygon/types/ohlc.types.ts
@@ -2,23 +2,30 @@ import { DateDayjsOrString } from "../../../../types/date.types";
 import { PolygonAggregateResult } from "./results.types";
 
 export type OHLCFetchOptions = {
-    adjusted?: boolean;
-    date: DateDayjsOrString;
+	adjusted?: boolean;
+	date: DateDayjsOrString;
 };
 
 export interface SnapshotResult extends PolygonAggregateResult {
-    T: string;
+	T: string;
 }
 
 // see https://polygon.io/docs/stocks/get_v2_aggs_grouped_locale_us_market_stocks__date
 export type OHLCFetchResponse = {
-    queryCount: number;
-    resultsCount: number;
-    adjusted: boolean;
-    results: Array<SnapshotResult>;
-    status: string;
-    request_id: string;
-    count: number;
+	queryCount: number;
+	resultsCount: number;
+	adjusted: boolean;
+	results: SnapshotResult[];
+	status: string;
+	request_id: string;
+	count: number;
 };
 
-export type OHLC = "ticker" | "timestamp" | "open" | "high" | "low" | "close" | "volume";
+export type OHLC =
+	| "ticker"
+	| "timestamp"
+	| "open"
+	| "high"
+	| "low"
+	| "close"
+	| "volume";
