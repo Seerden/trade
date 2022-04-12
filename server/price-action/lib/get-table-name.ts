6d21e52451a6ebba7d21609f4679f9925e8553b7
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-12 17:52:55 +0200

    (refactor:types/aggregate.types) move PermittedTimespan type from get-table-name, (code style) run prettier, (doc) reword a few comments

--- server/price-action/lib/get-table-name.ts
+++ server/price-action/lib/get-table-name.ts
@@ -1,11 +1,11 @@
-export type PermittedTimespan = "minute" | "hour" | "day";
+import { PermittedTimespan } from "./polygon/types/aggregate.types";
 
 export function timescaleToTableName(timespan: PermittedTimespan) {
-    const tableName: string = {
-        minute: "price_action_1m",
-        hour: "price_action_1h",
-        day: "price_action_1d",
-    }[timespan];
+	const tableName: string = {
+		minute: "price_action_1m",
+		hour: "price_action_1h",
+		day: "price_action_1d",
+	}[timespan];
 
-    return tableName;
+	return tableName;
 }
--- server/price-action/lib/polygon/types/aggregate.types.ts
+++ server/price-action/lib/polygon/types/aggregate.types.ts
@@ -1,36 +1,40 @@
 import { DateDayjsOrString } from "../../../../types/date.types";
-import { PermittedTimespan } from "../../get-table-name";
 import { PolygonAggregateResults } from "./results.types";
 
-// @dev -- note that these are all admissible timespans. for our purposes, we'll only be using minute, hour, day
+/**
+ * @note We'll probably only be using 'minute', 'hour', 'day'.
+ */
 export type PolygonTimespan =
-    | "minute"
-    | "hour"
-    | "day"
-    | "week"
-    | "month"
-    | "quarter"
-    | "year";
+	| "minute"
+	| "hour"
+	| "day"
+	| "week"
+	| "month"
+	| "quarter"
+	| "year";
+
+/**
+ * As mentioned, most valid timespans aren't relevant for us, for now.
+ */
+export type PermittedTimespan = "minute" | "hour" | "day";
 
 export type PolygonAggregateOptions = {
-    ticker: string;
-    multiplier: number;
-    /*  polygon actually allows more timespans (see PolygonTimespan type above)
-        but we don't care for any of those, at least not at this point */
-    timespan: PermittedTimespan;
-    from: DateDayjsOrString;
-    to: DateDayjsOrString;
-    adjusted?: boolean;
-    sort?: "asc" | "desc";
-    limit?: number;
+	ticker: string;
+	multiplier: number;
+	timespan: PermittedTimespan;
+	from: DateDayjsOrString;
+	to: DateDayjsOrString;
+	adjusted?: boolean;
+	sort?: "asc" | "desc";
+	limit?: number;
 };
 
 export type PolygonAggregateResponse = {
-    ticker: string;
-    adjusted: boolean;
-    queryCount: number;
-    request_id: string;
-    resultsCount: number;
-    status: string;
-    results: PolygonAggregateResults;
+	ticker: string;
+	adjusted: boolean;
+	queryCount: number;
+	request_id: string;
+	resultsCount: number;
+	status: string;
+	results: PolygonAggregateResults;
 };
