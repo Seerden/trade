ab281b09722cc4efb6312a2962b52ddd81868b4e
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-13 00:38:19 +0200

    (refactor:snapshot/inselumns->fields, extract query text to variable, use objectToArray)

--- server/price-action/lib/polygon/requests/snapshot/insert.ts
+++ server/price-action/lib/polygon/requests/snapshot/insert.ts
@@ -1,27 +1,33 @@
+import { objectToArray } from "helpers/object-to-array";
 import format from "pg-format";
+import { PriceActionRow } from "types/database.types";
 import { PriceActionApiObject } from "../../../../../database/pools/query-objects";
 import { DateDayjsOrString } from "../../../../../types/date.types";
 import { fetchDailyOHLC } from "./fetch";
 import { snapshotToPriceAction } from "./transform";
 
-export async function fetchAndInsertSnapshot({ date }: { date: DateDayjsOrString }) {
+export async function fetchAndInsertSnapshot({
+	date,
+}: {
+	date: DateDayjsOrString;
+}) {
 	const rawResponse = await fetchDailyOHLC({ date });
 	const priceActionObjects = snapshotToPriceAction(rawResponse);
 	if (!priceActionObjects.length) return;
 
-	const columns = "ticker, timestamp, open, close, high, low, volume".split(", ");
-	const priceActionObjectsAsArrays = priceActionObjects.map((row) =>
-		// @ts-ignore Fix the typing on priceActionObjects and then on row[column]
-		columns.map((column) => row[column])
+	const fieldsString = "ticker, timestamp, open, close, high, low, volume";
+	const fields = fieldsString.split(", ") as Array<keyof PriceActionRow>;
+	const priceActionObjectsAsArrays = priceActionObjects.map((object) =>
+		objectToArray(object, fields)
+	);
+
+	const text = format(
+		"insert into price_action_1d (%s) values %L returning timestamp",
+		fieldsString,
+		priceActionObjectsAsArrays
 	);
 
 	return PriceActionApiObject.query({
-		text: format(
-			`
-                insert into price_action_1d (ticker, timestamp, open, close, high, low, volume) values %L 
-                returning timestamp
-            `,
-			priceActionObjectsAsArrays
-		),
+		text,
 	}) as unknown;
 }
