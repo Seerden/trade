d2d82780d418e07fdb0d1a274a3bd210f0e97783
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-12 16:15:16 +0200

    (refactor:polygon/snapshot/fetch) use axios return type instead of as alias, (doc) fix wrong tense in comment

--- server/price-action/lib/polygon/requests/snapshot/fetch.ts
+++ server/price-action/lib/polygon/requests/snapshot/fetch.ts
@@ -4,11 +4,11 @@ import { OHLCFetchOptions, OHLCFetchResponse } from "../../types/ohlc.types";
 
 /**
  * Fetch daily OHLCV for all tickers. Note that we're on a free plan, so we can
- * only fetch data for a given date if that date's trading session is fully ended.
+ * only fetch data for a given date if that date's trading session has fully ended.
  */
 export async function fetchDailyOHLC({ date }: OHLCFetchOptions) {
 	const formattedDate = dayjs(date).format("YYYY-MM-DD");
 	const url = `/v2/aggs/grouped/locale/us/market/stocks/${formattedDate}`;
-	const { data } = await axiosPolygon.get(url);
-	return data as OHLCFetchResponse;
+	const { data } = await axiosPolygon.get<OHLCFetchResponse>(url);
+	return data;
 }
