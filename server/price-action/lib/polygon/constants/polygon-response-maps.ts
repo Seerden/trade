367761d767f65ea08faac07c5c76c0f107b70bcd
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-12 13:43:02 +0200

    (chore) rename response-mapping constants file to polygon-response-maps, (doc) and reword/update a few comments

---
+++ server/price-action/lib/polygon/constants/polygon-response-maps.ts
@@ -0,0 +1,21 @@
+/**
+ * @see: https://polygon.io/docs/stocks/get_v2_aggs_grouped_locale_us_market_stocks__date
+ * Polygon response keys (see link above) are not very descriptive, so let's map them for ease of use
+ */
+
+export const aggregateResponseKeyMap = {
+	c: "close",
+	h: "high",
+	l: "low",
+	n: "transactionCount",
+	o: "open",
+	t: "timestamp",
+	v: "volume",
+	vw: "vwap",
+};
+
+export const priceActionColumns = "close high low open timestamp volume".split(" ");
+
+// Snapshot response is similar to aggregate response, except there's an
+// additional relevant T field, which represents the ticker.
+export const snapshotResponseKeyMap = { T: "ticker", aggregateResponseKeyMap };
--- server/price-action/lib/polygon/requests/aggregate/aggregate-to-rows.ts
+++ server/price-action/lib/polygon/requests/aggregate/aggregate-to-rows.ts
@@ -4,9 +4,9 @@
 
 import { PriceActionRow } from "../../../../../types/database.types";
 import {
-    aggregateResponseKeyMap,
-    priceActionColumns,
-} from "../../constants/response-mapping";
+	aggregateResponseKeyMap,
+	priceActionColumns,
+} from "../../constants/polygon-response-maps";
 import { PolygonAggregateResponse } from "../../types/aggregate.types";
 import { PolygonAggregateResult } from "../../types/results.types";
 
@@ -15,53 +15,53 @@ import { PolygonAggregateResult } from "../../types/results.types";
  * @todo move this somewhere else, like a /validation folder
  */
 export function isValidResponse(response: PolygonAggregateResponse): boolean {
-    /* @todo: write functionality to verify that we receive a response shape from poly API,
+	/* @todo: write functionality to verify that we receive a response shape from poly API,
         in case they ever decide to change their API responses */
-    return response && true;
+	return response && true;
 }
 
 /**
  * Take a price action row and set its ticker property to an uppercase ticker
  */
 function withTicker(row: PriceActionRow, ticker: string) {
-    // eslint-disable-next-line no-param-reassign
-    row.ticker = ticker.toUpperCase();
-    return row;
+	// eslint-disable-next-line no-param-reassign
+	row.ticker = ticker.toUpperCase();
+	return row;
 }
 
 /**
  * Map one PolygonAggregateResult to a PriceActionRow
  */
 function mapAggregateResultToPriceAction(result: PolygonAggregateResult, ticker: string) {
-    const res: PriceActionRow = Object.keys(result).reduce(
-        (acc, key: keyof typeof aggregateResponseKeyMap) => {
-            const mappedKey = aggregateResponseKeyMap[key] as keyof PriceActionRow;
+	const res: PriceActionRow = Object.keys(result).reduce(
+		(acc, key: keyof typeof aggregateResponseKeyMap) => {
+			const mappedKey = aggregateResponseKeyMap[key] as keyof PriceActionRow;
 
-            if (priceActionColumns.includes(mappedKey)) {
-                const value = result[key];
+			if (priceActionColumns.includes(mappedKey)) {
+				const value = result[key];
 
-                (acc[mappedKey] as string | number) = value;
-                return acc;
-            }
+				(acc[mappedKey] as string | number) = value;
+				return acc;
+			}
 
-            return acc;
-        },
-        {} as PriceActionRow
-    );
+			return acc;
+		},
+		{} as PriceActionRow
+	);
 
-    return withTicker(res, ticker);
+	return withTicker(res, ticker);
 }
 
 /**
  * Transform a raw polygon /aggregate response to `PriceActionRow`s
  */
 export function aggregateToPriceActionObjects(
-    response: PolygonAggregateResponse
+	response: PolygonAggregateResponse
 ): Array<PriceActionRow> {
-    if (!isValidResponse(response)) return [];
+	if (!isValidResponse(response)) return [];
 
-    const { results, ticker } = response;
-    return results.map((result) => mapAggregateResultToPriceAction(result, ticker));
+	const { results, ticker } = response;
+	return results.map((result) => mapAggregateResultToPriceAction(result, ticker));
 }
 
 export type valueof<T> = T[keyof T];
