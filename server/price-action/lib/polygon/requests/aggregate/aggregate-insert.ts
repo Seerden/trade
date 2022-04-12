3ab8f9c0ffb5d74bf9c753cfb68b43ded0b31810
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-12 13:44:05 +0200

    (chore) rename aggregate-fetch file to just fetch

--- server/price-action/lib/polygon/requests/aggregate/aggregate-insert.ts
+++ server/price-action/lib/polygon/requests/aggregate/aggregate-insert.ts
@@ -7,40 +7,40 @@ import { storeFetchedDateRange } from "../../../../store/store-fetched-dates";
 import { PermittedTimespan, timescaleToTableName } from "../../../get-table-name";
 import { PolygonAggregateOptions } from "../../types/aggregate.types";
 import { OHLC } from "../../types/ohlc.types";
-import { fetchTickerAggregate } from "./aggregate-fetch";
 import { aggregateToPriceActionObjects } from "./aggregate-to-rows";
+import { fetchTickerAggregate } from "./fetch";
 
 /**
  * Map a timespan like 'minute' to a timescale/interval like '1m'
  */
 function timespanToTimescale(timespan: PermittedTimespan): Timescale {
-    const mapping: { [K in PermittedTimespan]: Timescale } = {
-        minute: "1m",
-        hour: "1h",
-        day: "1d",
-    };
+	const mapping: { [K in PermittedTimespan]: Timescale } = {
+		minute: "1m",
+		hour: "1h",
+		day: "1d",
+	};
 
-    return mapping[timespan];
+	return mapping[timespan];
 }
 
 export function priceActionObjectsToDatabaseRows(
-    priceActionObjects: ReturnType<typeof aggregateToPriceActionObjects>
+	priceActionObjects: ReturnType<typeof aggregateToPriceActionObjects>
 ) {
-    const rowsForDatabase = priceActionObjects.map((row) => {
-        /* make sure columns are in the same order as the columns in our query 
+	const rowsForDatabase = priceActionObjects.map((row) => {
+		/* make sure columns are in the same order as the columns in our query 
             (see format() call in fetchAndInsertAggregate) */
-        const columns = "ticker, timestamp, open, close, high, low, volume".split(
-            ", "
-        ) as Array<OHLC>;
-        return columns.map((column) => row[column]);
-    });
+		const columns = "ticker, timestamp, open, close, high, low, volume".split(
+			", "
+		) as Array<OHLC>;
+		return columns.map((column) => row[column]);
+	});
 
-    return rowsForDatabase;
+	return rowsForDatabase;
 }
 
 type InsertArgs = {
-    rowsForDatabase: ReturnType<typeof aggregateToPriceActionObjects>;
-    options: Pick<PolygonAggregateOptions, "ticker" | "from" | "to" | "timespan">;
+	rowsForDatabase: ReturnType<typeof aggregateToPriceActionObjects>;
+	options: Pick<PolygonAggregateOptions, "ticker" | "from" | "to" | "timespan">;
 };
 
 /**
@@ -49,28 +49,28 @@ type InsertArgs = {
  * and save the rows to table price_action_<1m|1h|1d>
  */
 export async function insertAggregateIntoDatabase(
-    rowsForDatabase: InsertArgs["rowsForDatabase"],
-    { ticker, from, to, timespan }: InsertArgs["options"]
+	rowsForDatabase: InsertArgs["rowsForDatabase"],
+	{ ticker, from, to, timespan }: InsertArgs["options"]
 ) {
-    const timestampsInserted: Array<{ timestamp: string | number }> =
-        await PriceActionApiObject.query({
-            text: format(
-                "insert into %I (ticker, timestamp, open, close, high, low, volume) values %L returning (timestamp)",
-                timescaleToTableName(timespan),
-                rowsForDatabase
-            ),
-        });
+	const timestampsInserted: Array<{ timestamp: string | number }> =
+		await PriceActionApiObject.query({
+			text: format(
+				"insert into %I (ticker, timestamp, open, close, high, low, volume) values %L returning (timestamp)",
+				timescaleToTableName(timespan),
+				rowsForDatabase
+			),
+		});
 
-    if (timestampsInserted.length) {
-        return storeFetchedDateRange({
-            ticker,
-            timescale: timespanToTimescale(timespan),
-            start: from,
-            end: to,
-        });
-    }
+	if (timestampsInserted.length) {
+		return storeFetchedDateRange({
+			ticker,
+			timescale: timespanToTimescale(timespan),
+			start: from,
+			end: to,
+		});
+	}
 
-    return [];
+	return [];
 }
 
 /**
@@ -79,45 +79,45 @@ export async function insertAggregateIntoDatabase(
  * - add fetched date range to redis store
  */
 export async function fetchAndInsertAggregate({
-    timespan,
-    ticker,
-    from,
-    to,
+	timespan,
+	ticker,
+	from,
+	to,
 }: {
-    timespan: PermittedTimespan;
-    ticker: string;
-    from: PolygonAggregateOptions["from"];
-    to: PolygonAggregateOptions["to"];
+	timespan: PermittedTimespan;
+	ticker: string;
+	from: PolygonAggregateOptions["from"];
+	to: PolygonAggregateOptions["to"];
 }) {
-    const rawResponse = await fetchTickerAggregate({
-        ticker,
-        from,
-        to,
-        timespan,
-        multiplier: 1,
-    });
-    // convert priceActionObjects to arrays for multi-insert with pg-promise
-    const rowsForDatabase = priceActionObjectsToDatabaseRows(
-        aggregateToPriceActionObjects(rawResponse)
-    );
+	const rawResponse = await fetchTickerAggregate({
+		ticker,
+		from,
+		to,
+		timespan,
+		multiplier: 1,
+	});
+	// convert priceActionObjects to arrays for multi-insert with pg-promise
+	const rowsForDatabase = priceActionObjectsToDatabaseRows(
+		aggregateToPriceActionObjects(rawResponse)
+	);
 
-    const timestampsInserted: Array<{ timestamp: string | number }> =
-        await PriceActionApiObject.query({
-            text: format(
-                "insert into %I (ticker, timestamp, open, close, high, low, volume) values %L returning (timestamp)",
-                timescaleToTableName(timespan),
-                rowsForDatabase
-            ),
-        });
+	const timestampsInserted: Array<{ timestamp: string | number }> =
+		await PriceActionApiObject.query({
+			text: format(
+				"insert into %I (ticker, timestamp, open, close, high, low, volume) values %L returning (timestamp)",
+				timescaleToTableName(timespan),
+				rowsForDatabase
+			),
+		});
 
-    if (timestampsInserted.length) {
-        return storeFetchedDateRange({
-            ticker,
-            timescale: timespanToTimescale(timespan),
-            start: from,
-            end: to,
-        });
-    }
+	if (timestampsInserted.length) {
+		return storeFetchedDateRange({
+			ticker,
+			timescale: timespanToTimescale(timespan),
+			start: from,
+			end: to,
+		});
+	}
 
-    return [];
+	return [];
 }
