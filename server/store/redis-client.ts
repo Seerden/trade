2212967b12c87e7937805d266f854061983fa746
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-12 15:14:54 +0200

    (redis) don't use legacyMode

--- server/store/redis-client.ts
+++ server/store/redis-client.ts
@@ -3,25 +3,28 @@ import session from "express-session";
 import { createClient } from "redis";
 
 export const redisClient = createClient({
-    url: "redis://store:6379", // note that `store` is the name we give to the redis service in docker-compose.yml
-    legacyMode: true,
+	url: "redis://store:6379", // note that `store` is the name we give to the redis service in docker-compose.yml
 });
 
 export const RedisStore = connectRedis(session);
 
 export const redisSession: session.SessionOptions = {
-    store: new RedisStore({ client: redisClient }),
-    saveUninitialized: false,
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    cookie: {
-        maxAge: 7 * 24 * 3600 * 1000, // max age is a week by default
-        secure: process.env.NODE_ENV === "production",
-    },
+	store: new RedisStore({ client: redisClient }),
+	saveUninitialized: false,
+	secret: process.env.SESSION_SECRET,
+	resave: false,
+	cookie: {
+		maxAge: 7 * 24 * 3600 * 1000, // max age is a week by default
+		secure: process.env.NODE_ENV === "production",
+	},
 };
 
 export async function startRedis() {
-    await redisClient.connect();
-    redisClient.on("connected", () => console.log("connected"));
-    redisClient.on("error", (e) => console.log(e));
+	try {
+		await redisClient.connect();
+		redisClient.on("connected", () => console.log("connected"));
+		redisClient.on("error", (e) => console.log(e));
+	} catch (error) {
+		console.log({ message: "Error connecting to redis client", error });
+	}
 }
