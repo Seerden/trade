60b5063e83148374ed7f0e8d9e115ca2878771bf
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-13 12:53:58 +0200

    (refactor:snapshot/insert) extract insertSnapshot function from fetchAndInsertSnapshot, rename fields to priceActionFields and put variables in a constants file, (type) change arguments for fetchAndInsertSnapshot to non-object

--- server/price-action/lib/polygon/requests/snapshot/insert.ts
+++ server/price-action/lib/polygon/requests/snapshot/insert.ts
@@ -1,40 +1,45 @@
-import { objectToArray } from "helpers/object-to-array";
 import format from "pg-format";
 import { PriceActionRow } from "types/database.types";
 import { PriceActionApiObject } from "../../../../../database/pools/query-objects";
+import { objectToArray } from "../../../../../helpers/object-to-array";
 import { DateDayjsOrString } from "../../../../../types/date.types";
-import { fetchDailyOHLC } from "./fetch";
+import {
+	priceActionFields,
+	priceActionFieldsString,
+} from "../../../constants/fields";
+import { fetchSnapshot } from "./fetch";
 import { snapshotToPriceAction } from "./transform";
 
-// TODO: these can be in a /constants file - just make sure to refine variable
-// names first.
-const fieldsString = "ticker, timestamp, open, close, high, low, volume";
-const fields = fieldsString.split(", ") as Array<keyof PriceActionRow>;
+/**
+ * Insert a polygon daily snapshot into the database.
+ * @param priceActionObjects this is the return from `snapshotToPriceAction()`
+ */
+async function insertSnapshot(priceActionObjects: PriceActionRow[]) {
+	if (!priceActionObjects.length) return;
+
+	const priceActionArrays = priceActionObjects.map((object) =>
+		objectToArray(object, priceActionFields)
+	);
+
+	const text = format(
+		"insert into price_action_1d (%s) values %L returning timestamp",
+		priceActionFieldsString,
+		priceActionArrays
+	);
+
+	const queryResponse = await PriceActionApiObject.query({ text });
+	return queryResponse;
+}
 
 /**
  * Fetch daily OHLCV snapshot for all tickers, convert the response to priceAction
  * rows, then insert these rows into our database.
  */
-export async function fetchAndInsertSnapshot({
-	date,
-}: {
-	date: DateDayjsOrString;
-}) {
-	const rawResponse = await fetchDailyOHLC({ date });
+export async function fetchAndInsertSnapshot(date: DateDayjsOrString) {
+	const rawResponse = await fetchSnapshot({ date });
 
 	const priceActionObjects = snapshotToPriceAction(rawResponse);
-
 	if (!priceActionObjects.length) return;
 
-	const priceActionObjectsAsArrays = priceActionObjects.map((object) =>
-		objectToArray(object, fields)
-	);
-
-	const text = format(
-		"insert into price_action_1d (%s) values %L returning timestamp",
-		fieldsString,
-		priceActionObjectsAsArrays
-	);
-
-	return PriceActionApiObject.query({ text });
+	return await insertSnapshot(priceActionObjects);
 }
