088b87540ba92a037d6be2d87ebc53a5e926c2b2
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-14 01:02:54 +0200

    (refactor:aggregate/fetch) extract getAggregateUrl to function for later reusability

--- server/price-action/lib/polygon/requests/aggregate/fetch.ts
+++ server/price-action/lib/polygon/requests/aggregate/fetch.ts
@@ -5,6 +5,26 @@ import {
 	PolygonAggregateResponse,
 } from "../../types/aggregate.types";
 
+interface AggregateUrlOptions {
+	ticker: string;
+	multiplier: string | number;
+	timespan: string;
+	from: string;
+	to: string;
+}
+
+/** Build URL to use for a Polygon.io aggregate GET request. */
+function getAggregateUrl({
+	ticker,
+	multiplier,
+	timespan,
+	from,
+	to,
+}: AggregateUrlOptions) {
+	const url = `/v2/aggs/ticker/${ticker.toUpperCase()}/range/${multiplier}/${timespan}/${from}/${to}`;
+	return url;
+}
+
 /**
  * Fetch aggregate data for a ticker in a given time-range. Note that using a date like
  * '2022-01-15' will include all data until the end of that day's trading session.
@@ -19,7 +39,13 @@ export async function fetchTickerAggregate({
 	limit = 50000,
 }: PolygonAggregateOptions) {
 	const [fromString, toString] = [from, to].map((date) => formatYMD(date));
-	const url = `/v2/aggs/ticker/${ticker.toUpperCase()}/range/${multiplier}/${timespan}/${fromString}/${toString}`;
+	const url = getAggregateUrl({
+		ticker,
+		multiplier,
+		timespan,
+		from: fromString,
+		to: toString,
+	});
 	const { data } = await axiosPolygon.get<PolygonAggregateResponse>(url, {
 		params: { limit, sort },
 	});
