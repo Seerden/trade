93d5c849d52727ba95db7ebe30b4e3237b3a3ccd
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-15 01:11:30 +0200

    (doc:formatYMD) update docstring to highlight no return on invalid date, (type) add Maybe to return type

--- server/price-action/lib/time/format-YMD.ts
+++ server/price-action/lib/time/format-YMD.ts
@@ -1,8 +1,11 @@
 import dayjs from "dayjs";
 import { DateDayjsOrString } from "../../../types/date.types";
 
-/** Given a date, format to YYYY-MM-DD. */
-export function formatYMD(date: DateDayjsOrString) {
+/**
+ * Given a date, format to YYYY-MM-DD.
+ * @note Doesn't return anything if dayjs deems the date an "Invalid Date"
+ */
+export function formatYMD(date: DateDayjsOrString): Maybe<string> {
 	const formattedDate = dayjs(date).format("YYYY-MM-DD");
 
 	if (!(formattedDate === "Invalid Date")) return formattedDate;
