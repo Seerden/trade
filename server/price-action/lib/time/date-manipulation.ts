4f849c5a1ad5eb31c1f605647c90e8a55570dcf7
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-19 00:35:10 +0200

    (doc:date-manipulation) clean up function docstring

--- server/price-action/lib/time/date-manipulation.ts
+++ server/price-action/lib/time/date-manipulation.ts
@@ -9,16 +9,18 @@ dayjs.extend(timezone);
 
 /**
  * Convert a date to a UNIX timestamp, e.g. for use with yf API
- * Note that the yf API expects a timestamp in seconds, whereas in JS we typically work with milliseconds
+ * Note that the yf API expects a timestamp in seconds, whereas in JS we
+ * typically work with milliseconds.
  */
 export function dateToTimestamp(date: DateDayjsOrString) {
 	return dayjs(date).valueOf() / 1000;
 }
 
 /**
- * Take a date and convert it to US market time (i.e. New York local time) as a string
- * To get something usable as New York time, we have to convert to a string, otherwis dayjs will,
- * with most methods, end up using the host machine's locale again.
+ * Take a date and convert it to US market time (i.e. New York local time) as a
+ * string. To get something usable as New York time, we have to convert to a
+ * string, otherwise dayjs will, with most methods, end up using the host
+ * machine's locale again.
  */
 export function toNewYorkTime(
 	date: DateDayjsOrString,
