3371d1e93088d3d5c9a2792a0d9e54e3116e6d0b
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-18 21:35:47 +0200

    (chore/docs) update docstring for a few functions in check-date helper file

--- server/price-action/lib/time/check-date.ts
+++ server/price-action/lib/time/check-date.ts
@@ -3,23 +3,20 @@ import { DateDayjsOrString } from "../../../types/date.types";
 import { formatYMD } from "./format-YMD";
 import { marketHolidays } from "./market-holidays";
 
-/**
- * Check if a date is a workday (workdays are Monday through Friday)
- */
+/** Check if a date is a workday (workdays are Monday through Friday). */
 export function isWorkday(date: DateDayjsOrString) {
 	const dayOfWeek = dayjs(date).day();
 	return dayOfWeek > 0 && dayOfWeek < 6;
 }
 
-/**
- * Check whether a date (date can be at any time throughout the day) is a market holiday
- */
+/** Check whether a date (date can be at any time throughout the day) is a market holiday. */
 export function isHoliday(date: DateDayjsOrString) {
 	return marketHolidays.includes(formatYMD(date));
 }
 
 /**
- * Check whether a date (`date` be set to any time of day) is an active market day
+ * Check whether a date (`date` can be set to any time of day) is an active market
+ * day. Returns `false` on weekends and market holidays.
  */
 export function isActiveMarketDay(date: DateDayjsOrString) {
 	return isWorkday(date) && !isHoliday(date);
