674a61195a34e9d35ff2dcdc754c8f2c977472a0
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-19 00:40:57 +0200

    (chore:most-active) clean up comment on a dayjs tz implementation, and expand on market half-days in docstring

--- server/price-action/database/queries/most-active.ts
+++ server/price-action/database/queries/most-active.ts
@@ -13,23 +13,13 @@ dayjs.extend(tz);
 
 /**
  * Convert a date to a timestamp corresponding to the market close of that day.
- * Doesn't account for weekends and holidays, just gets us a timestamp for 4PM
- * on that day.
+ * Doesn't account for non-trading days, but does recognize half-days.
  */
 export function dateToEODTimestamp(date: DateDayjsOrString) {
 	const ymdDate = formatYMD(date);
 
 	const closeTime = isEarlyClose(date) ? "13:00" : "16:00";
 
-	// Using dayjs.tz(date, timezone) actually gets us the time for that date
-	// in that timezone. Doing something like dayjs(date).tz(timezone) doesn't
-	// do the same thing.
-
-	// dayjs(date).valueOf() === dayjs(date).tz(timezone).valueOf(),
-	// whereas dayjs(date).valueOf() !== dayjs.tz(date, timezone) (unless
-	// 'timezone' matches the machine's timezone).
-
-	// So, to be clear: the below statement actually gets us the timestamp for market close.
 	const timestamp = dayjs(
 		dayjs.tz(`${ymdDate} ${closeTime}`, "America/New_York")
 	).valueOf();
