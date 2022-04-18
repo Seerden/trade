1ed084e636da00bfcb0168dc1339ba5f295f3862
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-18 21:36:54 +0200

    (chore+feat/market-day-range) run prettier, update docstring and rename a few variables in listMarketDaysInRange helper, and implement a listMarketDaysInRange helper that.. lists all market days in a range

--- server/price-action/lib/time/market-day-range.ts
+++ server/price-action/lib/time/market-day-range.ts
@@ -1,6 +1,8 @@
-import dayjs, { Dayjs } from "dayjs";
+import { captureMessage } from "@sentry/node";
+import dayjs from "dayjs";
 import { DateDayjsOrString } from "../../../types/date.types";
 import { isActiveMarketDay } from "./check-date";
+import { formatYMD } from "./format-YMD";
 
 /**
  * Given `start`, find the date `n-1` market days after `start`.
@@ -16,37 +18,64 @@ import { isActiveMarketDay } from "./check-date";
  * param, and polygon expects dates formatted like this.
  */
 export function nMarketDayRange({
-    n,
-    start,
-    end,
+	n,
+	start,
+	end,
 }: {
-    n: number;
-    start?: DateDayjsOrString;
-    end?: DateDayjsOrString;
+	n: number;
+	start?: DateDayjsOrString;
+	end?: DateDayjsOrString;
 }): string[] | undefined {
-    if (n === 0) return undefined;
+	if (n === 0) return;
 
-    // can't specify both start and end, so use `start` if both are specified
-    const givenEndpoint = start || end;
-    const endpointStartOfDay = dayjs(givenEndpoint).startOf("day");
+	// Can't specify both start and end, so use `start` if both are specified.
+	const endpoint = dayjs(start || end);
 
-    if (n === 1) return [endpointStartOfDay.format("YYYY-MM-DD")];
+	if (n === 1) return [formatYMD(endpoint)];
 
-    /* if `end` specified, we want to walk back, so need to add -1 days per step,
+	/* if `end` specified, we want to walk back, so need to add -1 days per step,
         hence need sign === -1 */
-    const sign = start ? 1 : -1;
+	const sign = start ? 1 : -1;
 
-    let numMarketDaysInInterval = isActiveMarketDay(endpointStartOfDay) ? 1 : 0;
-    let otherIntervalEndpoint: Dayjs = endpointStartOfDay;
+	let runningDateCount = isActiveMarketDay(endpoint) ? 1 : 0;
+	let nextDate = endpoint;
 
-    // walk (backwards or forwards) n-1 days
-    while (numMarketDaysInInterval < n) {
-        otherIntervalEndpoint = otherIntervalEndpoint.add(sign * 1, "day");
-        if (isActiveMarketDay(otherIntervalEndpoint)) {
-            numMarketDaysInInterval += 1;
-        }
-    }
+	// Walk (backwards or forwards) n-1 days
+	while (runningDateCount < n) {
+		nextDate = nextDate.add(sign * 1, "day");
+		if (isActiveMarketDay(nextDate)) {
+			runningDateCount += 1;
+		}
+	}
 
-    const range = start ? [start, otherIntervalEndpoint] : [otherIntervalEndpoint, end];
-    return range.map((date) => dayjs(date).format("YYYY-MM-DD"));
+	const range = start ? [start, nextDate] : [nextDate, end];
+	return range.map((date) => formatYMD(date));
+}
+
+/**
+ * Given a `start` and `end` date, list all market days between (and including)
+ * `start` and `end`.
+ */
+export function listMarketDaysInRange(
+	start: DateDayjsOrString,
+	end: DateDayjsOrString
+) {
+	if (dayjs(formatYMD(end)).valueOf() <= dayjs(formatYMD(start)).valueOf()) {
+		captureMessage("listMarketDaysInRange: provided end <= start.");
+		return;
+	}
+
+	const dates = [];
+
+	let currentDate = dayjs(start).startOf("day");
+
+	while (currentDate <= dayjs(end).startOf("day")) {
+		if (isActiveMarketDay(currentDate)) {
+			dates.push(formatYMD(currentDate));
+		}
+
+		currentDate = currentDate.add(1, "day");
+	}
+
+	return dates;
 }
