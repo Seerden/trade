e55158db8f1ad9a6edfbdd024874e2871d4b8cbd
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-20 01:52:27 +0200

    (dev:dev-router) add a few dev endpoints: list polygonQueue jobs, trigger bulk queue of aggregate backlog jobs

--- server/api/routers/dev-router.ts
+++ server/api/routers/dev-router.ts
@@ -7,6 +7,11 @@ import { readFile } from "fs/promises";
 import { getDailyMostActive } from "../../price-action/database/queries/most-active";
 import { fetchSnapshotWithLimiter } from "../../price-action/lib/polygon/requests/snapshot/fetch";
 import { writeAggregateTickerDateTuplesToFetchToFile } from "../../price-action/lib/queue/aggregate/aggregate-backlog";
+import { queueAggregateBacklog } from "../../price-action/lib/queue/aggregate/queue-backlog";
+import {
+	listPolygonQueueJobs,
+	polygonQueue,
+} from "../../price-action/lib/queue/polygon-queue";
 import { addSnapshotFetchJobs } from "../../price-action/lib/queue/snapshot/add-fetch-job";
 import { redisClient } from "../../store/redis-client";
 
@@ -44,6 +49,16 @@ devRouter.get("/aggregate-backlog", async (req, res) => {
 	res.json({ writtenFile: JSON.parse(writtenFile) });
 });
 
+devRouter.get("/aggregate-backlog/queue", async (req, res) => {
+	if (!(await redisClient.get("aggregate:backlog:queued"))) {
+		const queuedJobs = await queueAggregateBacklog();
+		redisClient.set("aggregate:backlog:queued", 1);
+		res.json({ queuedJobs });
+	} else {
+		res.json({ jobs: await polygonQueue.getJobs() });
+	}
+});
+
 devRouter.get("/queue-snapshot-fetch-manually", async (req, res) => {
 	res.json({ addedJobs: await addSnapshotFetchJobs(["2022-01-22"]) });
 });
@@ -52,4 +67,8 @@ devRouter.get("/snapshot/raw/:date", async (req, res) => {
 	res.json({
 		response: await fetchSnapshotWithLimiter({ date: req.params.date }),
 	});
+});
+
+devRouter.get("/queue/polygon/jobs", async (req, res) => {
+	res.json({ jobs: await listPolygonQueueJobs() });
 });
