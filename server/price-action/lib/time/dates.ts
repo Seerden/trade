f66679067e9bbfe199da390b4088221233708097
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-19 20:16:47 +0200

    (feat:dates) implement function to get nearest past finished market date relative to given date

--- server/price-action/lib/time/dates.ts
+++ server/price-action/lib/time/dates.ts
@@ -1,5 +1,7 @@
 import dayjs, { Dayjs } from "dayjs";
-import { isHoliday, isWorkday } from "./check-date";
+import { DateDayjsOrString } from "../../../types/date.types";
+import { dateToEODTimestamp } from "../../database/queries/most-active";
+import { isActiveMarketDay, isHoliday, isWorkday } from "./check-date";
 import { formatYMD } from "./format-YMD";
 
 // get list of all market days in the past two years
@@ -20,4 +22,47 @@ export function getAllMarketDaysInPastTwoYears() {
 	}
 
 	return dates.map((date) => formatYMD(date));
+}
+
+/**
+ * Get the date corresponding to the nearest (in the past) finished trading
+ * session. If date is either (1) a market date, but the session is still
+ * active, or (2) not a market date at all, walk back in time until we find
+ * another market day.
+ */
+export function getNearestPastFinishedMarketDay(
+	date: DateDayjsOrString = new Date()
+) {
+	const initialDate = dayjs(date);
+	const initialTimestamp = initialDate.valueOf();
+	const eodTimestamp = dateToEODTimestamp(initialDate);
+	const eodToday = dateToEODTimestamp(new Date());
+
+	// Handle case where given a future date.
+	if (initialTimestamp > eodToday) {
+		// If trading session has ended, return today.
+		if (!(eodToday > new Date().valueOf())) {
+			return formatYMD(new Date());
+		}
+		// Handle case where we're passed a timestamp on a trading day that's
+		// already ended.
+	} else if (initialTimestamp > eodTimestamp) {
+		return formatYMD(initialDate);
+	}
+
+	let currentDate = dayjs(initialDate).add(-1, "day");
+	while (
+		// if date not market date, keep walking back
+		!isActiveMarketDay(currentDate) ||
+		// if date is after today, keep walking back
+		dayjs(formatYMD(currentDate)).valueOf() >
+			dayjs(formatYMD(new Date())).valueOf() ||
+		// if date is today and today's session not ended, keep walking back
+		(formatYMD(currentDate) === formatYMD(new Date()) &&
+			currentDate.valueOf() < dateToEODTimestamp(currentDate))
+	) {
+		currentDate = currentDate.add(-1, "day");
+	}
+
+	return formatYMD(currentDate);
 }
