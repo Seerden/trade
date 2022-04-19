bac1ab0a178806e6c75027ec6fba6a16a30d631a
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-19 20:17:17 +0200

    (test) implement tests for getNearestPastFinishedMarketDay

--- server/price-action/lib/time/tests/dates.test.ts
+++ server/price-action/lib/time/tests/dates.test.ts
@@ -1,6 +1,9 @@
 import dayjs from "dayjs";
 import { isHoliday, isWorkday } from "../check-date";
-import { getAllMarketDaysInPastTwoYears } from "../dates";
+import {
+	getAllMarketDaysInPastTwoYears,
+	getNearestPastFinishedMarketDay,
+} from "../dates";
 import { formatYMD } from "../format-YMD";
 
 describe("getAllMarketDaysInPastTwoYears", () => {
@@ -20,5 +23,22 @@ describe("getAllMarketDaysInPastTwoYears", () => {
 		expect(result).toEqual(expect.arrayContaining(expected));
 		expect(result.length).toBeGreaterThan(400);
 		expect(result.length).toBeLessThan(600);
+	});
+});
+
+describe("getNearestFinishedMarketDay", () => {
+	test("returns correct value if passed a regular trading day that's still in session", () => {
+		const result = getNearestPastFinishedMarketDay("2022-04-19 19:58");
+		expect(result).toEqual("2022-04-18");
+	});
+
+	test("returns correct value if given easter sunday", () => {
+		const result = getNearestPastFinishedMarketDay("2022-04-17 15:00");
+		expect(result).toEqual("2022-04-14");
+	});
+
+	test("returns correct result if given a date in the future", () => {
+		const result = getNearestPastFinishedMarketDay("2022-05-15");
+		expect(result).toEqual("2022-04-18");
 	});
 });
