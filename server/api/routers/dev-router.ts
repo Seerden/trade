69f86f1011c573670f191e323800400bcfbfbaed
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-24 16:48:12 +0200

    (tweak:queue-router) move polygonQueue-related endpoints from devRouter to queueRouter, and add an endpoint to fetch job data by job id

--- server/api/routers/dev-router.ts
+++ server/api/routers/dev-router.ts
@@ -9,10 +9,7 @@ import { getDailyMostActive } from "../../price-action/database/queries/most-act
 import { fetchSnapshotWithLimiter } from "../../price-action/lib/polygon/requests/snapshot/fetch";
 import { writeAggregateTickerDateTuplesToFetchToFile } from "../../price-action/lib/queue/aggregate/aggregate-backlog";
 import { queueAggregateBacklog } from "../../price-action/lib/queue/aggregate/queue-backlog";
-import {
-	listPolygonQueueJobs,
-	polygonQueue,
-} from "../../price-action/lib/queue/polygon-queue";
+import { polygonQueue } from "../../price-action/lib/queue/polygon-queue";
 import { addSnapshotFetchJobs } from "../../price-action/lib/queue/snapshot/add-fetch-job";
 import { redisClient } from "../../store/redis-client";
 import { snapshotRouter } from "./snapshot-router";
@@ -69,16 +66,6 @@ devRouter.get("/queue-snapshot-fetch-manually", async (req, res) => {
 devRouter.get("/snapshot/raw/:date", async (req, res) => {
 	res.json({
 		response: await fetchSnapshotWithLimiter({ date: req.params.date }),
-	});
-});
-
-devRouter.get("/queue/polygon/jobs", async (req, res) => {
-	res.json({ jobs: await listPolygonQueueJobs() });
-});
-
-devRouter.get("/queue/polygon/jobs/count/delayed", async (req, res) => {
-	res.json({
-		delayedJobCount: await polygonQueue.getJobCountByTypes("delayed"),
 	});
 });
 
--- server/api/routers/queue-router.ts
+++ server/api/routers/queue-router.ts
@@ -1,3 +1,31 @@
 import { Router } from "express";
+import {
+	listPolygonQueueJobs,
+	polygonQueue,
+} from "../../price-action/lib/queue/polygon-queue";
 
 export const queueRouter = Router({ mergeParams: true });
+
+/** Get all jobs tagged 'delayed' from polygonQueue. */
+queueRouter.get("/queue/polygon/jobs/count/delayed", async (req, res) => {
+	res.json({
+		delayedJobCount: await polygonQueue.getJobCountByTypes("delayed"),
+	});
+});
+
+/** Fetch counts and data for a few types of jobs. */
+queueRouter.get("/queue/polygon/jobs", async (req, res) => {
+	res.json({ jobs: await listPolygonQueueJobs() });
+});
+
+/** Get data for the job matching `jobId` */
+queueRouter.get("/polygon/job/:id", async (req, res) => {
+	const jobId = req.params.id;
+	const job = await polygonQueue.getJob(jobId);
+
+	if (!job) {
+		res.json({ message: `No job found with id ${jobId}` });
+	} else {
+		res.json({ job });
+	}
+});
