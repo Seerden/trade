1673c89f90a855d5df02d50120523bcdda7342ec
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-24 18:56:25 +0200

    (style:NewTicket/sub) move StyledHeader definition to sub.style file

--- client/src/components/NewTicket/NewTickets/sub/Header.tsx
+++ client/src/components/NewTicket/NewTickets/sub/Header.tsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import { StyledHeader } from "./sub.style";
 
 export default function Header() {
 	const columns = "Action Ticker Price Quantity Date Time".split(" ");
@@ -16,23 +16,3 @@ export default function Header() {
 		</StyledHeader>
 	);
 }
-
-const StyledHeader = styled.div`
-	display: grid;
-	// TODO: these column widths and gap size follow those from the NewTicket
-	// component (specifically, the StyledInput in there). Should combine those
-	// styles with these here so that they're always in sync. Would also make it
-	// easier to manage a responsive version of this component.
-	grid-template-columns: repeat(4, 5rem) 9rem 6.5rem;
-	gap: ${({ theme }) => theme.padding.tiny};
-
-	label {
-		user-select: none;
-		text-align: center;
-		font-size: ${({ theme }) => theme.font.smaller};
-		font-weight: 500;
-		border-bottom: 2px solid #eee;
-		padding-bottom: ${(p) => p.theme.padding.tinier};
-		margin-bottom: ${(p) => p.theme.padding.tinier};
-	}
-`;
--- client/src/components/NewTicket/NewTickets/sub/sub.style.tsx
+++ client/src/components/NewTicket/NewTickets/sub/sub.style.tsx
@@ -92,3 +92,23 @@ export const StyledTradeActionLabel = styled.label<{
 			border-bottom: 2px solid orange;
 		`}
 `;
+
+export const StyledHeader = styled.div`
+	display: grid;
+	// TODO: these column widths and gap size follow those from the NewTicket
+	// component (specifically, the StyledInput in there). Should combine those
+	// styles with these here so that they're always in sync. Would also make it
+	// easier to manage a responsive version of this component.
+	grid-template-columns: repeat(4, 5rem) 9rem 6.5rem;
+	gap: ${({ theme }) => theme.padding.tiny};
+
+	label {
+		user-select: none;
+		text-align: center;
+		font-size: ${({ theme }) => theme.font.smaller};
+		font-weight: 500;
+		border-bottom: 2px solid #eee;
+		padding-bottom: ${(p) => p.theme.padding.tinier};
+		margin-bottom: ${(p) => p.theme.padding.tinier};
+	}
+`;
