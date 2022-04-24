01b23442cdf78dbd79410b902a6f5de5d20ef6f6
Christiaan Seerden <christiaanseerden@live.nl>
2022-04-24 19:14:18 +0200

    (cleanup:NewTickets/Input) remove unused RequiredInput component

--- client/src/components/NewTicket/NewTickets/sub/Input.tsx
+++ client/src/components/NewTicket/NewTickets/sub/Input.tsx
@@ -8,8 +8,3 @@ type Props = Partial<
 export default function Input({ $size, ...inputProps }: Props) {
 	return <StyledInput {...{ ...inputProps, $size }} />;
 }
-
-/** RequiredInput is exactly the same as Input, except we set required=true. */
-export function RequiredInput(...props: Parameters<typeof Input>) {
-	return <Input {...props} required />;
-}
